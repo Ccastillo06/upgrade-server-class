e6cf66e1f6405c1fca599dd575f041cf1e900a02
Ccastillo06 <ccastillon92@gmail.com>
2019-06-22 12:56:50 +0200

    Add jwt authentication and secure graphql route

--- app.js
+++ app.js
@@ -5,22 +5,26 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
+const passport = require('passport');
 
 const authRoutes = require('./routes/auth');
 const schema = require('./schema');
 
 const app = express();
 
+app.use(cors());
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use('/auth', authRoutes);
-
 app.use(
   '/graphql',
+  passport.authenticate('jwt', { session: false }),
   graphqlHTTP({
     schema,
     graphiql: true
